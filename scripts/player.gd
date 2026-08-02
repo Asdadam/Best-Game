25d48b2a5b476545e0d91296de1e635ffd273b49
Mehmet Üren <urenmehmet796@gmail.com>
2026-08-02 16:44:55 +0300

    Duvara yapışıkken zıplayamamam bugu fixlendi
    sonunda aw

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,12 +7,14 @@ const WALL_SLIDE_SPEED : float = 40.0
 const WALL_CLIMB_SPEED : float = -90.0
 const WALL_JUMP_VELOCITY : Vector2 = Vector2(220.0, -280.0)
 const WALL_JUMP_LOCK_TIME : float = 0.1
+const COYOTE_TIME : float = 0.2
 
 @onready var dash_duration: Timer = $Dash_duration
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var dashing : bool = false
 var wall_jump_timer : float = 0.0
+var coyote_timer : float = 0.0
 
 func _ready() -> void:
 	dash_duration.timeout.connect(_on_dash_timer_timeout)
@@ -21,22 +23,24 @@ func _physics_process(delta: float) -> void:
 	if wall_jump_timer > 0:
 		wall_jump_timer -= delta
 
+
 	var direction := Input.get_axis("move_left", "move_right")
 
-	
 	var wall_normal := get_wall_normal().x
 	var is_against_wall := is_on_wall() and ((direction > 0 and wall_normal < 0) or (direction < 0 and wall_normal > 0))
 
 
 	if not is_on_floor():
-
-		if is_against_wall:
+		coyote_timer -= delta
+		if is_against_wall and coyote_timer <= 0:
 			if Input.is_action_pressed("WallClimb"):
 				velocity.y = WALL_CLIMB_SPEED 
 			else:
 				velocity.y = WALL_SLIDE_SPEED 
 		else:
 			velocity.y += get_gravity().y * delta
+	else:
+		coyote_timer =COYOTE_TIME
 
 
 	if Input.is_action_just_pressed("Jump"):
