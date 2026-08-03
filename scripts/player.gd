f830fa11d923ce75e9d46dfcecd132ffe606429a
Mehmet Üren <urenmehmet796@gmail.com>
2026-08-03 15:54:28 +0300

    animasyonlar
    animasyonlar değişti,attack animasyonu eklendi

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+#Değişkenler
 const SPEED : float = 130.0
 const JUMP_VELOCITY : float = -300.0
 const DASH_SPEED : float = 260.0
@@ -9,25 +10,41 @@ const WALL_JUMP_VELOCITY : Vector2 = Vector2(220.0, -280.0)
 const WALL_JUMP_LOCK_TIME : float = 0.1
 const COYOTE_TIME : float = 0.2
 
+
+
 @onready var dash_duration: Timer = $Dash_duration
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var hit_box: Area2D = $Pivot/HitBox
+@onready var pivot: Node2D = $Pivot
+
+
 
 var dashing : bool = false
-var can_dash : float = true
+var can_dash : bool = true
 var wall_jump_timer : float = 0.0
 var coyote_timer : float = 0.0
+var is_attacking : bool = false
+
+
 
 func _ready() -> void:
 	dash_duration.timeout.connect(_on_dash_timer_timeout)
+	animation_player.animation_finished.connect(_on_animation_finished)
 
 func _physics_process(delta: float) -> void:
+	
+	if animated_sprite.flip_h:
+		pivot.scale.x = -1
+	else:
+		pivot.scale.x = 1
+	
 	if wall_jump_timer > 0:
 		wall_jump_timer -= delta
 
-
-	var direction := Input.get_axis("move_left", "move_right")
-
-	var wall_normal := get_wall_normal().x
+	var direction := Input.get_axis("move_left", "move_right")#Oyuncunun gittiği taraf
+	var wall_normal := get_wall_normal().x#Duvarın normali
+	#Duvara yapışmışsak duvarın normaliile ters yöne bakmalıyız
 	var is_against_wall := is_on_wall() and ((direction > 0 and wall_normal < 0) or (direction < 0 and wall_normal > 0))
 	
 	if is_against_wall:
@@ -36,7 +53,7 @@ func _physics_process(delta: float) -> void:
 		can_dash = true
 
 	if not is_on_floor():
-		coyote_timer -= delta
+		coyote_timer -= delta#Zıpladıktan sonra hemen duvara yapışmamak için araya küçük bir süre sıkıştırdım
 		if is_against_wall and coyote_timer <= 0:
 			if Input.is_action_pressed("WallClimb"):
 				velocity.y = WALL_CLIMB_SPEED 
@@ -62,6 +79,7 @@ func _physics_process(delta: float) -> void:
 
 
 	if dashing:
+		#Yerimzide durmuyorsak gittiğimiz yöne dash,yerimizde duruyorsak baktığımız yere dash atmak için Dash_speed ile çarpacağımız vektör
 		var dash_dir = direction if direction != 0 else (-1.0 if animated_sprite.flip_h else 1.0)
 		velocity.x = dash_dir * DASH_SPEED
 	elif wall_jump_timer <= 0:
@@ -75,15 +93,30 @@ func _physics_process(delta: float) -> void:
 	elif direction > 0 and wall_jump_timer <= 0:
 		animated_sprite.flip_h = false
 
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		is_attacking = true
+		animation_player.play("attack")
+		animated_sprite.play("attack")
+
+	if not is_attacking:
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
+			if velocity.y > 0:
+				animated_sprite.play("falling")
+			else:
+				animated_sprite.play("jumping")
+			
+
 
 	move_and_slide()
 
 func _on_dash_timer_timeout() -> void:
 	dashing = false
+
+func _on_animation_finished(anim_name : StringName):
+	if anim_name == "attack":
+		is_attacking = false
